bc7402d7d1ddf6e45bd3a2e7757d65c5659384f4
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-05-30 22:52:01 +0000

    4 lines

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:66], end="")
-print(str[-23:-17] + str[:6])
+print(str[39:66] + str[-23:-17] + str[:6])
