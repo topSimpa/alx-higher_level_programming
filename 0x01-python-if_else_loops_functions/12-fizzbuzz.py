f575adb21bcab0a3423fa1e9d2c70dd35be183cd
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-01 03:42:13 +0000

    loop and conditions

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    count = 1
+    while count <= 100:
+        if count % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif count % 5 == 0:
+            print("Buzz", end=" ")
+        elif count % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(count, end=" ")
+        count += 1
+fizzbuzz()
