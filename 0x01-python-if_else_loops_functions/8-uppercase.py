9f9b58c8c17e671dd8b0cb75a5080635cff48772
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-01 01:48:55 +0000

    looping function concat

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newStr = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            newStr += chr(ord(char) + 32)
+        else:
+            newStr += char
+    return newStr
