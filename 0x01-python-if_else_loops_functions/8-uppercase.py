1cd579b9679055b1107ac408b15e0e89f300079a
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-01 01:58:53 +0000

    looping string ord chr

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,10 @@ def uppercase(str):
     newStr = ""
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-            newStr += chr(ord(char) + 32)
+            newStr += chr(ord(char) - 32)
         else:
             newStr += char
     return newStr
+
+
+print(uppercase("mJ3eeb"))
