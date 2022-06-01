31e5c22b9cec714ea0a7b586c5e0a32b44b5f394
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-01 04:09:02 +0000

    slicing for only positive

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newStr = str[:n] + str[n+1:]
-    return newStr
+    if n >= 0:
+        newStr = str[:n] + str[n+1:]
+        return newStr
+    else:
+        return str
