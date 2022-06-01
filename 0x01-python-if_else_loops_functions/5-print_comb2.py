f0c7354d55f3745cd0352ef256e758f613a72faa
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-01 00:16:02 +0000

    styling correct

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 num = 0
 while num < 100:
     if num != 99:
-    	print("{:02d},".format(num), end=' ')
+        print("{:02d},".format(num), end=' ')
     else:
         print("{:02d}".format(num))
     num += 1
