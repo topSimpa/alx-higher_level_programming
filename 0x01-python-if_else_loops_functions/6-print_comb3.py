135bde20a116851bce2dd0f0a161451f23e53562
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-01 00:56:48 +0000

    less pair first

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+fDigit = 0
+while fDigit < 10:
+    sDigit = 0
+    while sDigit < 10:
+        if sDigit >= fDigit:
+            if not (fDigit == 9 and sDigit == 9):
+                print("{}{},".format(fDigit, sDigit), end=" ")
+            else:
+                print("{}{}".format(fDigit, sDigit))
+        sDigit += 1
+    fDigit += 1
