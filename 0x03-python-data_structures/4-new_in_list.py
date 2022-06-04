7e13cc58d16476f4739f1eed2d5c2525efd51000
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-04 13:01:24 +0000

    copy and replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (my_list):
+        new_list = []
+        for i in my_list:
+            new_list.append(i)
+        if idx >= 0 and idx < len(my_list):
+            new_list[idx] = element
+        return (new_list)
