6facbd9b6f6875f614f7f8d0f0cd91016d7cea0b
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-04 13:30:24 +0000

    5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        withoutc = ""
+        for char in my_string:
+            if char != 'c' and char != 'C':
+                withoutc += char
+        return withoutc
