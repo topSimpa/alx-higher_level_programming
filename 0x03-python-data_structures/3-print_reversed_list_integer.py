eb93c787551e440ceec9aee6a693578cd0f9258e
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-04 12:26:58 +0000

    if null no printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    lent = len(my_list)
-    while lent > 0:
-        print("{:d}".format(my_list[lent - 1]))
-        lent -= 1
+    if (my_list):
+        lent = len(my_list)
+        while lent > 0:
+            print("{:d}".format(my_list[lent - 1]))
+            lent -= 1
