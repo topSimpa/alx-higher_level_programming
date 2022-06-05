3c6055fdc5bb93fe6b76c36d63b550f2b6f4dc48
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-05 13:30:46 +0000

    just string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
+    if my_string and type(my_string) == str:
         withoutc = ""
         for char in my_string:
             if char != 'c' and char != 'C':
