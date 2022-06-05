2c18a425c1993435dc7c0014aa7fc31f432ecbbe
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-05 22:52:53 +0000

    even for tuple greater than 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     sum_one, sum_two = 0, 0
-    if len(tuple_a) == 2:
+    if len(tuple_a) >= 2:
         sum_one += tuple_a[0]
         sum_two += tuple_a[1]
     elif len(tuple_a) == 1:
         sum_one += tuple_a[0]
 
-    if len(tuple_b) == 2:
+    if len(tuple_b) >= 2:
         sum_one += tuple_b[0]
         sum_two += tuple_b[1]
     elif len(tuple_b) == 1:
