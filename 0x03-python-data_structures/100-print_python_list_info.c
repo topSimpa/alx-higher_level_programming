2ef9df77273dc4cae8a7ab11c73243efdcaf538a
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-07 14:42:12 +0000

    let see if i can use cpython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,30 @@
+#include "Python.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <assert.h>
+#include "main.h"
+
+/**
+ *print_python_list_info - this print python list info
+ *
+ *@p: the Python list object
+ *Return: void
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	py_ssize_t len, elem = 0;
+
+	if (PyList_check(*P))
+	{
+		len = PyList_Size(*p)
+		printf("[*] Size of the Python List = %li", len);
+		for (; elem < len; elem++)
+		{
+			printf("Element %d: %s", elem, PyList_GetItem(elem));
+		}
+	}
+}
