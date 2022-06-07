31ab4fb03fa4f58e3a0d90bf6da4a095884faa97
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-07 18:59:33 +0000

    Cpython trial

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,4 @@
-#include <python3.4/Python.h>
+#include "Python.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,15 +16,35 @@
 
 void print_python_list_info(PyObject *p)
 {
-	py_ssize_t len, elem = 0;
+	Py_ssize_t len, elem = 0;
 
-	if (PyList_check(*P))
+	if (PyList_Check(p))
 	{
-		len = PyList_Size(*p)
-		printf("[*] Size of the Python List = %li", len);
-		for (; elem < len; elem++)
+		len = PyList_Size(p);
+		printf("[*] Size of the Python List = %li\n", len);
+		if (len > 0)
 		{
-			printf("Element %d: %s", elem, PyList_GetItem(elem));
+			for (; elem < len; elem++)
+			{
+				if (PyLong_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: int\n", elem);
+				else if (PyList_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: list\n", elem);
+				else if (PyBool_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: bool\n", elem);
+				else if (PyFloat_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: float\n", elem);
+				else if (PyComplex_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: complex\n", elem);
+				else if (PyTuple_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: tuple\n", elem);
+				else if (PyDict_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: dictionary\n", elem);
+				else if (PySet_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: set\n", elem);
+				else if (PyUnicode_Check(PyList_GetItem(p, elem)))
+					printf("Element %li: str\n", elem);
+			}
 		}
 	}
 }
---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,6 @@
+#ifndef _MAINH_H
+#define _MAINH_H
+
+void print_python_list_info(PyObject *p);
+
+#endif
