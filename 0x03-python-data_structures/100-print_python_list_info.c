cbe790c6d9906d18172c3b0c26c0a44bf51eaea6
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-08 03:26:40 +0000

    python object are c object that has the attribute allocated

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,11 +1,8 @@
-#include <python3.4/Python.h>
+#include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <limits.h>
-#include <assert.h>
-#include "main.h"
+#include "lists.h"
 
 /**
  *print_python_list_info - this print python list info
@@ -22,29 +19,28 @@ void print_python_list_info(PyObject *p)
 	{
 		len = PyList_Size(p);
 		printf("[*] Size of the Python List = %li\n", len);
-		if (len > 0)
+		printf("[*] Allocated = %li\n", ((PyListObject *)p)->allocated);
+
+		for (; elem < len; elem++)
 		{
-			for (; elem < len; elem++)
-			{
-				if (PyLong_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: int\n", elem);
-				else if (PyList_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: list\n", elem);
-				else if (PyBool_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: bool\n", elem);
-				else if (PyFloat_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: float\n", elem);
-				else if (PyComplex_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: complex\n", elem);
-				else if (PyTuple_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: tuple\n", elem);
-				else if (PyDict_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: dictionary\n", elem);
-				else if (PySet_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: set\n", elem);
-				else if (PyUnicode_Check(PyList_GetItem(p, elem)))
-					printf("Element %li: str\n", elem);
-			}
+			if (PyLong_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: int\n", elem);
+			else if (PyList_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: list\n", elem);
+			else if (PyBool_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: bool\n", elem);
+			else if (PyFloat_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: float\n", elem);
+			else if (PyComplex_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: complex\n", elem);
+			else if (PyTuple_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: tuple\n", elem);
+			else if (PyDict_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: dictionary\n", elem);
+			else if (PySet_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: set\n", elem);
+			else if (PyUnicode_Check(PyList_GetItem(p, elem)))
+				printf("Element %li: str\n", elem);
 		}
 	}
 }
