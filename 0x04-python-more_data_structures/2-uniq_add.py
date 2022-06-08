248018be2ce884b754aa13e3bc6ec9c6348a6e42
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-08 19:21:00 +0000

    uniq not single frequency

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if my_list:
-        uniq_list = [i for i in my_list if my_list.count(i) == 1]
+        uniq_list = []
+        for i in my_list:
+            if (i not in uniq_list):
+                uniq_list.append(i)
         return sum(uniq_list)
     return my_list
