99da8408dc02b0c78dfb29f4993a3b06b31f97a2
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-08 18:38:57 +0000

    using condition in list comprehension

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+
+        rep_list = [replace if elem == search else elem for elem in my_list]
+        return rep_list
