18e5caa4692eea6d5aca6f1824034e38a7189537
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-09 13:27:53 +0000

    dictionary is mutable

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+def multiply_by_2(a_dictionary):
+    times2_dict = {}
+    for key in a_dictionary.keys():
+        times2_dict[key] = a_dictionary[key] * 2
+    return times2_dict
