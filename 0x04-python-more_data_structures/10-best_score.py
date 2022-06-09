05e29be6b1591a0f292c0ddda2c6c275110e680d
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-09 14:54:29 +0000

    dictionary and loops

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        max = 0
+        for key in a_dictionary:
+            if a_dictionary[key] >= max:
+                max = a_dictionary[key]
+        return max
+    return None
