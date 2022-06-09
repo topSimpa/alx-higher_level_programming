26fb5b2ddb9cc0336a71f0fff877874685d17492
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-09 15:19:18 +0000

    keys

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max = 0
-        for key in a_dictionary:
+        for key in a_dictionary.keys():
             if a_dictionary[key] >= max:
                 max = a_dictionary[key]
         return max
