06aa65d3d2a65ad9f15b0cb44ab35a7f2f391a15
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-09 12:03:53 +0000

    styling issue solved

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for keys in sorted(a_dictionary.keys()):
-        print (f"{keys}: {a_dictionary[keys]}")
+        print(f"{keys}: {a_dictionary[keys]}")
