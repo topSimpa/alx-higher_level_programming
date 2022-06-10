79854af261f2d1ed82f514224497e39c83405e84
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-06-10 11:44:59 +0000

    name mistake

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if weight_average:
+    if my_list:
         prod_sum = list(map(lambda x: x[0] * x[1], my_list))
         weight_avg = sum(prod_sum) / sum([i[1] for i in my_list])
         return weight_avg
