cade97d5d20fc6f334b4efcd5ad01577efef5c07
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-06-22 13:45:00 +0000

    when format donot match type it is called ValueError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except IndexError:
             raise
-        except TypeError:
+        except (ValueError, TypeError):
             continue
     print("")
     return (count)
