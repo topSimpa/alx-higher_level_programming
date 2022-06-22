a49a5371b656f5d9159194ba0be404a12c0c38a7
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-06-22 13:31:57 +0000

    styling says exception should be specified

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     for index in range(x):
         try:
             print(my_list[index], end="")
             count += 1
-        except:
+        except IndexError:
             continue
     print("")
     return (count)
-
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for index in range(x):
@@ -8,8 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except IndexError:
             raise
-        except:
+        except TypeError:
             continue
     print("")
     return (count)
-
