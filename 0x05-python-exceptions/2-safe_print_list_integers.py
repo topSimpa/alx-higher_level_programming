7994c2e33890487b137ae8464f5059b78a34e0b9
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-06-22 13:10:49 +0000

    try and except multiple

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for index in range(x):
         try:
-            print(my_list[index], end="")
+            print("{:d}".format(my_list[index]), end="")
             count += 1
+        except IndexError:
+            raise
         except:
             continue
     print("")
