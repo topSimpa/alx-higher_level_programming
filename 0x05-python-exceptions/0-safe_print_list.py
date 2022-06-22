baace7919aca48cb5c9356024052f5914417cfa3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-06-22 11:36:56 +0000

    safe printing using try/except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    if (x and my_list):
+        count = 0
+        for index in range(x):
+            try:
+                print(my_list[index], end="")
+            except IndexError:
+                count += 1
+        print("")
+        return (x - count)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+try and except usage in python
