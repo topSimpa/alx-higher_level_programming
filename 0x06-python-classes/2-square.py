df25f6ea5ca8fb9fb5bd7c3f2afa0b985412b9bb
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-06-23 09:15:08 +0000

    the message can follow in a paranthesis after name

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,15 +10,9 @@ class Square:
     using the init function, condition to set size
     """
     def __init__(self, size=0):
-        if type(size) == int:
-            self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         elif size < 0:
-            try:
-                raise ValueError
-            except ValueError:
-                print("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
-            try:
-                raise TypeError
-            except TypeError:
-                print("size must be an integer")
+            self.__size = size
