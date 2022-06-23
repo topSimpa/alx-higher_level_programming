12d3b4e6c02a18f6898145ee7946fa3d7c492232
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-06-23 09:25:55 +0000

    adding a public method area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" we define a square class with one attribute
+size which is peculiar to different sqaure object
+size is always be an integer greater than 0
+"""
+
+
+class Square:
+    """This class initializes an attribute size on each instance
+    using the init function, condition to set size
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """This method returns the area of a square
+       which size ** size """
+
+    def area(self):
+        return (size * size)
