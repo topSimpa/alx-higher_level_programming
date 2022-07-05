9afe1c215f8123ca402b93a57aecd4a9bd53f68e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-05 02:53:46 +0000

    fix documentation problem

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""module defines a function that returns a list of attributes"""
+
+
 def lookup(obj):
+    """ returns all attribute and method of an object"""
     return (dir(obj))
