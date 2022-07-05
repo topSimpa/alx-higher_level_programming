954690e0f569f8f8dcb9d585f06cf200c265fa59
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-05 22:37:15 +0000

    add 6 a modification of 5 with area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" star a new class calle BaseGeometry """
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
+
+    def area(self):
+        """ raises an except """
+        raise Exception("area() is not implemented")
