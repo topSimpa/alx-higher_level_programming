d80bcc528fbfce39a60c07ea787a9a044ebecb41
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-06 14:12:49 +0000

    added if for when read is empty

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ def read_file(filename=""):
     if filename:
         with open(filename, "r", encoding="utf-8") as f:
             text = f.read().strip("\n")
-        print(text)
+        if text:
+            print(text)
