83935303953bf3c7451f0becb98458b04940ea55
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-06 22:49:17 +0000

    added a writing function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """This function writes to a file and returns word-count"""
     with open(filename, "w", encoding="utf-8") as f:
         return (f.write(text))
