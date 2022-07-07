32befc9eccee2bebcf36b1374a69de52ae98b834
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-07 16:31:57 +0000

    selective attribute printing

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" describe a class Student """
+
+
+class Student:
+    """ The student class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the json dict representation of an object"""
+        js = {}
+        if attrs:
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    js[i] = self.__dict__[i]
+            return (js)
+        return (self.__dict__)
