0e7a604b9c3f2c7c06e25896bd4a991c3b23669c
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-13 17:54:27 +0000

    test for . ? \ without space and with multi space taken care of

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,8 +6,10 @@ def text_indentation(text):
     """include 2 new lines where there is ., ?, and : """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.replace(". ", ".\n\n")
-    text = text.replace("? ", "?\n\n")
-    text = text.replace(": ", ":\n\n")
-    text = text.strip()
-    print(text, sep='')
+    text = text.replace(".", ".\n\n")
+    text = text.replace("?", "?\n\n")
+    text = text.replace(":", ":\n\n")
+    text_lines = text.split("\n\n")
+    text_lines = [i.strip()+"\n\n" for i in text_lines]
+    text = ''.join(text_lines)
+    print(text.strip(), end='')
