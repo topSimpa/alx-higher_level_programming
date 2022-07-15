78b81a5afa8846cb622d0c8468036ab32732929e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-15 21:20:37 +0000

    fix rectangle not triangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@
 from models.base import Base
 
 
-class Triangle(Base):
+class Rectangle(Base):
     """ prototype of triangle class, it a child of base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,7 +5,7 @@
 import os
 from unittest import TestCase
 from models.base import Base
-from models.rectangle import Triangle
+from models.rectangle import Rectangle
 
 
 class TriangleTestCases(TestCase):
@@ -14,28 +14,28 @@ class TriangleTestCases(TestCase):
     def test_instance(self):
         """ test if instances are instance of Base and Triangle"""
 
-        t = Triangle(2, 3)
+        t = Rectangle(2, 3)
         self.assertIsInstance(t, Base)
         self.assertIsInstance(t, Triangle)
 
     def test_width_height(self):
-        t = Triangle(2, 3)
+        t = Rectangle(2, 3)
         self.assertEqual(t.width, 2)
         self.assertEqual(t.height, 3)
         self.assertEqual(t.x, 0)
         self.assertEqual(t.y, 0)
 
     def test_all_arg(self):
-        t = Triangle(2, 3, 4, 5, 3)
+        t = Rectangle(2, 3, 4, 5, 3)
         self.assertEqual(t.width, 2)
         self.assertEqual(t.height, 3)
         self.assertEqual(t.x, 4)
         self.assertEqual(t.y, 5)
 
     def test_id_multiple_none(self):
-        t = Triangle(2, 3, 4, 5)
-        r = Triangle(4, 3, 4, 5)
-        i = Triangle(2, 1, 3, 5)
+        t = Rectangle(2, 3, 4, 5)
+        r = Rectangle(4, 3, 4, 5)
+        i = Rectangle(2, 1, 3, 5)
         self.assertEqual(r.id, t.id + 1)
         self.assertEqual(i.id, r.id + 1)
 
