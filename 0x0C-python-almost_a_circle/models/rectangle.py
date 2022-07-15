264e9d17ba7e31b737d9fdd88c359eaedd233ac4
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-15 21:13:06 +0000

    added rectangle prototype

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" defines the Triangle class """
+
+
+from models.base import Base
+
+
+class Triangle(Base):
+    """ prototype of triangle class, it a child of base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__()
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
