c428fc743c53770a250d03f1ac997380821f2718
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-07-16 22:05:34 +0000

    each method for one assert

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,56 +11,55 @@ from models.rectangle import Rectangle
 class TriangleTestCases(TestCase):
     """ the test cases for the triangle classes as a method """
 
-    def test_instance(self):
-        """ test if instances are instance of Base and Triangle"""
+    def test_instance_of_base(self):
+        """ test if instances are instance of Base and Rectangle"""
+        self.assertIsInstance(Rectangle(2, 3), Base)
 
-        t = Rectangle(2, 3)
-        self.assertIsInstance(t, Base)
-        self.assertIsInstance(t, Rectangle)
+    def test_instance_of_rectangle(self):
+        self.assertIsInstance(Rectangle(2, 3), Rectangle)
 
-    def test_width_height(self):
+    def test_width_rigth(self):
         t = Rectangle(2, 3)
         self.assertEqual(t.width, 2)
-        self.assertEqual(t.height, 3)
-        self.assertEqual(t.x, 0)
-        self.assertEqual(t.y, 0)
 
-    def test_all_arg(self):
-        t = Rectangle(2, 3, 4, 5, 3)
-        self.assertEqual(t.width, 2)
+    def test_height_right(self):
+        t = Rectangle(2, 3)
         self.assertEqual(t.height, 3)
-        self.assertEqual(t.x, 4)
-        self.assertEqual(t.y, 5)
 
     def test_id_multiple_none(self):
-        t = Rectangle(2, 3, 4, 5)
         r = Rectangle(4, 3, 4, 5)
         i = Rectangle(2, 1, 3, 5)
-        self.assertEqual(r.id, t.id + 1)
         self.assertEqual(i.id, r.id + 1)
 
 
-    def test_width(self):
+    def test_width_type_error(self):
         with self.assertRaises(TypeError):
             Rectangle(2.5, 2)
+
+    def test_width_value_error(self):
         with self.assertRaises(ValueError):
             Rectangle(-2, 1)
 
-    def test_height(self):
+    def test_height_type_error(self):
         with self.assertRaises(TypeError):
             Rectangle(2, 2.5)
+
+    def test_width_type_error(self):
         with self.assertRaises(ValueError):
             Rectangle(1, -2)
 
-    def test_x(self):
+    def test_x_type_error(self):
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 2.5)
+
+    def test_x_value_error(self):
         with self.assertRaises(ValueError):
             Rectangle(1, 2, -2)
 
-    def test_y(self):
+    def test_y_type_error(self):
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 1, -2.5)
+    def test_y_value_error(self):
         with self.assertRaises(ValueError):
             Rectangle(1, 2, 1, -2)
 
