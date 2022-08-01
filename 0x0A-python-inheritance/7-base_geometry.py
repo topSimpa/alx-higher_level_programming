dfa7c757fc7519cd90c0e7af8e0ca034a89bcd43
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-01 21:54:42 +0000

    value checker

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" star a new class calle BaseGeometry """
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
+
+    def area(self):
+        """ raises an except """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{name} must be greater than 0")
