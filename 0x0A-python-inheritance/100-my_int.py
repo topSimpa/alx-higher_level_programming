76bcb7c822c0fea6e13509616b44e723e735aaf3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-02 16:47:20 +0000

    rebel int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ defines a rebel MyInt """
 
+
 class MyInt(int):
     """a rebel integer class"""
 
     def __eq__(self):
-       if self == 3:
-           return (False)
-       return (True)
+        if self == 3:
+            return (False)
+        return (True)
 
     def __ne__(self):
         if self != 3:
-           return (False)
+            return (False)
         return (True)
