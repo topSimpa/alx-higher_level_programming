fa980fd0bfe28b53539ad239d4666da4064b556e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-02 18:59:24 +0000

    rebel int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,8 +4,8 @@
 
 class MyInt(int):
     """a rebel integer class"""
-
     def __eq__(self, value):
         return (super().__ne__(value))
+
     def __ne__(self, value):
         return (super().__eq__(value))
