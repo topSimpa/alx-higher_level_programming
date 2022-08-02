1e14959ba1093de71e9059fc7d6e250e9c3e2fb1
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-02 16:13:28 +0000

    stylish printing

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this defines the rectangle class"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ this class is a child of BaseGeometry"""
+
+    def __init__(self, size):
+        """initializes the class attribute"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ returns the area of Square"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+	return (f"[Square] {self.__size}/{self.__size}")
