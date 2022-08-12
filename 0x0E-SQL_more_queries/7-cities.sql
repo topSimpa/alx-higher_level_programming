1af902b392a0431cdafada4aa696eae7dd2160c3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-12 01:14:53 +0000

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- creates a database called hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`state_id`) REFERENCES states(id),
+	`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`state_id` INT NOT NULL,
+        `name` VARCHAR(256) NOT NULL
+);
+
