68f05d35239f56e2c4a83fe3888ec8abac6ba9da
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-12 01:39:18 +0000

    nested query using foreign key

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of California that can be found in the database
-SELECT id, name
+SELECT `id`, `name`
 FROM `hbtn_0d_usa`.`cities`
-WHERE state_id = (SELECT id
-		WHERE name = "California"
-);
+WHERE `state_id` = (SELECT `id`
+		WHERE `name` = "California")
+ORDER BY `id` ASC
