ef53e108f664481ac4e37b9cb479134918607187
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-12 00:59:20 +0000

    constraint everywhere

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates a database called hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
+	PRIMARY KEY(`id`),
+	`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+         `name` VARCHAR(256) NOT NULL
+);
+
