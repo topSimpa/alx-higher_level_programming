5aad3205674dcae999646f1b76356740309971a9
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-13 10:15:53 +0000

    this is it

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT c.id, c.name, s.name
 FROM cities AS c
-FULL JOIN states as s
-ON c.state_id = s.id
+FULL OUTER JOIN states as s
+ON c.id = s.id
 ORDER BY c.id;
