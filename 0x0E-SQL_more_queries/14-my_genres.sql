d1cbae845cce0e3fc38e4b26d8248a5898fe61fd
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-14 09:16:52 +0000

    join on the  first join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- lists all shows contained in hbtn_0d_tvshows
 SELECT name
 FROM tv_genres
-WHERE id IN
-	(SELECT tv_show_genres.genre_id
-	FROM tv_shows
+INNER JOIN
+	(tv_shows
 	INNER JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_shows.title = "Dexter")
+	ON tv_shows.id = tv_show_genres.show_id)
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = "Dexter"
 ORDER BY name ASC;
