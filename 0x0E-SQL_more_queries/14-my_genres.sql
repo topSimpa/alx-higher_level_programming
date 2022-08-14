02608caab6a73fceb436af905a36c062b949b60c
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-08-14 09:05:27 +0000

    here it is

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,10 @@
 -- lists all shows contained in hbtn_0d_tvshows
-SELECT tv_show_genres.name
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.name == "Dexter"
-ORDER BY tv_show_genres.name ASC;
+SELECT name
+FROM tv_genres
+WHERE id IN
+	(SELECT tv_show_genres.genre_id
+	FROM tv_shows
+	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = "Dexter")
+ORDER BY name ASC;
