5a302bf998f109b2a414ceb6b7ddf748894a2413
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-06 08:58:30 +0000

    command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const process = require('process')
-if (process.argv.length < 3){
-	console.log('No argument')
-}else if (process.argv.length == 3){
-	console.log('Argument found')
-}else{
-	console.log('Arguments found')
+const process = require('process');
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
