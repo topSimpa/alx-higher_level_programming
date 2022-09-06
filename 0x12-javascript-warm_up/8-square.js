63f1850a4836701ca52ae65924950c1487736ed1
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-06 20:25:15 +0000

    new method str.repeat = 'x' * n in python

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+if (parseInt(process.argv[2])) {
+  let x = parseInt(process.argv[2]);
+  let y = x;
+  while (x > 0) {
+    console.log('X'.repeat(y));
+    x--;
+  }
+} else {
+  console.log('Missing size');
+}
