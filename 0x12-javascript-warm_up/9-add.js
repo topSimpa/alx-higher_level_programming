44e28a255e2605085a72a208a2d4a0ffa66e894b
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-07 07:16:44 +0000

    functions

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const process = require('process');
 
 function add (a, b) {
-  return (a * b);
+  return (a + b);
 }
 
-console.log(add(process.argv[2], process.argv[3]));
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
