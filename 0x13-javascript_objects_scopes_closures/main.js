933d41d13fbb21f2b3e8e5ac37e57091f194620f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-08 17:34:27 +0000

    inheritance in javascript

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+const Square = require('./5-square');
 
-const r1 = new Rectangle(4, 5);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-r1.print();
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
