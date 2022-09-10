fb0ed5a113c22589af772e477dcf60fed5577bd5
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-10 14:38:43 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" This query a database for a table called states for all entry"""
+""" This query a database for all entry in table called states
+    whose name column matches the fourt argument to this script
+
+    Usage: ./2-filtetr_states.py <username> <passwd> <database>
+            <state name>
+"""
 
 
 import MySQLdb
@@ -10,8 +15,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
                          password=arg[2], db=arg[3], charset="utf8")
     cur = db.cursor()
-    cur.execute(f"SELECT *  FROM states ORDER BY id ASC")
+    cur.execute("SELECT * \
+                 FROM states \
+                 WHERE name = '{}' \
+                 ORDER BY id ASC".format(arg[4])
+                )
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == arg[4]:
-            print(row)
+        print(row)
