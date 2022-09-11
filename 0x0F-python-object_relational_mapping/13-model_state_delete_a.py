ac088a15fcae5d6292d1a870fda505756cd3c6c3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-11 20:33:47 +0000

    deleting done

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""connect to database AND
+   delete all state
+   that has 'a' in it name
+
+   Usage: ./9-model_state_delete_a.py <username> <passwd> <database name>
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        if 'a' in instance.name:
+            session.delete(instance)
+    session.commit()
