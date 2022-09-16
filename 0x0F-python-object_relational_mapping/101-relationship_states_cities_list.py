556be6809b38ec5ca48a64b9dcfffd1ec79b12cb
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-16 13:54:34 +0000

    the relationship order

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""connect to database AND
+   list all State in the database
+
+   Usage: ./7-model_state_fetch_all.py <username> <passwd> <database name>
+"""
+
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
