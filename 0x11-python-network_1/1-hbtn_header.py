4ea3268d0181a04d1423103dc572c6a6e885f37b
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-23 21:05:38 +0000

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script fetches a url passed to it as an argument:
+   and displays the value of the X-Request-Id variable
+   found in the header of the response
+"""
+import sys
+from urllib.request import urlopen
+
+with urlopen(sys.argv[1]) as request:
+    print(request.getheader('X-Request-Id'))
