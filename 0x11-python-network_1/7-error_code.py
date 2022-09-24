d9daae49da42ced051ee22a13885f8815a6f29d3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-24 02:39:48 +0000

    error with requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL
+   and displays the body of the response (decoded in utf-8)
+
+   Usage: ./7-error_code <url>
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    resp = requests.get(sys.argv[1])
+    if resp.status_code >= 400:
+        print(f"Error code: {resp.status_code}")
+    else:
+        print(resp.text)
