2f3a5efbb7c85ff0a1876f5af1c94ffd1574841e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-24 02:27:47 +0000

    post with lib requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     import requests
 
     dat = {'email': sys.argv[2]}
-    req = requests.post(sys.argv[1], data=data)
+    req = requests.post(sys.argv[1], data=dat)
     print(req.text)
