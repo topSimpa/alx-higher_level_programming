efa88c675c0ffd767d039e78a974ce80cb9efc66
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-24 02:24:46 +0000

    post with lib requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This script that takes in a URL and an email,
+   sends a POST request to the passed URL with the email as a parameter,
+   and displays the body of the response
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    dat = {'email': sys.argv[2]}
+    req = requests.get(sys.argv[1], data=data)
+    print(request.text)
