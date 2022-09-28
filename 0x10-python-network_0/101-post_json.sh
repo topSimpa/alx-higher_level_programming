69df61a2b1e4d2d2da46185d4cc00bd7b36a0b59
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-28 00:51:08 +0000

    json trier

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #print the body of the response with a special header sent in request 
-curl -s -X POST --data-urlencode @"$2" "$1"
+curl -sL -X POST -H "Content-type:application/json" -d @"$2" "$1"
